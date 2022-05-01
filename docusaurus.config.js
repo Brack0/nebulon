64c75375070bb0cd3c3335162cb32393db005050
Brack0 <brack@localhost.localdomain>
2022-05-01 22:54:16 +0200

    first article

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,6 +48,9 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: "dark",
+      },
       navbar: {
         title: "brack0.dev",
         logo: {
