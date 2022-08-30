821d4640bb4f03d638eff6ca26b868394d21c51f
brack0 <denis.souron@laposte.net>
2022-08-31 00:48:14 +0200

    Add og:image for sharing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import Head from "@docusaurus/Head";
 import { translate } from "@docusaurus/Translate";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 import HomepageDescription from "@site/src/components/HomepageDescription";
 import Layout from "@theme/Layout";
 import React from "react";
@@ -9,11 +11,18 @@ export default function Home(): JSX.Element {
     <Layout
       title={`127.0.0.1`}
       description={translate({
-        id: "homepage.tagline",
-        message: "The Rebel Alliance's blog vs. the Microservice Empire",
-        description: "The homepage tagline",
+        id: "homepage.meta.description",
+        message:
+          "The Rebel Alliance's blog vs. the Microservice Empire. Tales of a developer, casual blogger & co-host of Front-end chronicles podcast.",
+        description: "The meta description for homepage",
       })}
     >
+      <Head>
+        <meta
+          property="og:image"
+          content={useBaseUrl("/img/nebulon_preview.png")}
+        />
+      </Head>
       <HomepageHeader />
       <main>
         <HomepageDescription />
